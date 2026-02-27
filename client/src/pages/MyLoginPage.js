15bd6f26ee46acab3203c2b7cf05253b942b2853
phoebef64 <phoebefranklin64@utexas.edu>
2026-02-27 12:01:39 -0600

    Initial code for login page, user portal, and navbar on home page in App to get to all pages

---
+++ client/src/pages/MyLoginPage.js
@@ -0,0 +1,20 @@
+// Author: Phoebe Franklin 2/23/2026
+function Login() {
+ return (
+    <div>
+        <h2>Username:</h2>
+      <input 
+        type="text"
+        placeholder="Username"
+      />
+        <h2>Password:</h2>
+            <input 
+        type="text"
+        placeholder="Password"
+      />
+    </div>
+  );
+}
+
+
+export default Login;
