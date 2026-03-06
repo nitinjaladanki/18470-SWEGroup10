ad20985faec2d1720869ca4e6b523946bfaf555a
phoebef64 <phoebefranklin64@utexas.edu>
2026-03-06 01:25:33 -0600

    Login page + user portal updated to match existing css, routing between login page and forgot passowrd page added

--- client/src/pages/MyUserPortal.js
+++ client/src/pages/MyUserPortal.js
@@ -1,32 +1,78 @@
 // Author: Phoebe Franklin 2/23/2026
+
+import { useState } from "react";
+import styles from "./MyUserPortal.module.css";
+
 function Portal() {
+  
+  const [name, setName] = useState("");
+  const [projectID, setProjectID] = useState("");
+  const [projectDesc, setProjectDesc] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("New Project Created: ", name);
+    setSubmitted(true);
+  };
+
  return (
-    <div>
-        <h1>Projects</h1>
-        <ul>
-        <li>Project 1</li>
-        <li>Project 2</li>
-        <li>Project 3</li>
-        </ul>
 
-        <h1>Create New Project</h1>
-                <h2>Project Name:</h2>
-      <input 
-        type="text"
-        placeholder="Name"
-      />
-              <h2>ProjectID:</h2>
-      <input 
-        type="text"
-        placeholder="ProjectID"
-      />
-              <h2>Project Description:</h2>
-      <input 
-        type="text"
-        placeholder="Description"
-        size = "50"
-      />
-
+<div className={styles.container}>
+  <div className={styles.card}>
+     <h2>My Project View</h2>
+     <div className={styles.projectcard}style={{ backgroundColor: "#005f86" }} >
+        <h2>My Project 1</h2>
+        
+    </div>
+         <div className={styles.projectcard}style={{ backgroundColor: "#bf5700" }} >
+        <h2>My Project 2</h2>
+        
+    </div>
+         <div className={styles.projectcard}style={{ backgroundColor: "#579d42" }} >
+        <h2>My Project 3</h2>
+        
+    </div>
+  </div>
+      <div className={styles.card}>
+        <h2 className={styles.title}>Create New Project</h2>
+        {submitted ? (
+          <p>
+            Your new project has been created. 
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <p>Enter your new project info here:</p>
+            <input
+              type="name"
+              required
+              placeholder="Project Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className={styles.input}
+            />
+            <input
+              type="projectID"
+              required
+              placeholder="Project ID"
+              value={projectID}
+              onChange={(e) => setProjectID(e.target.value)}
+              className={styles.input}
+            />
+            <input
+              type="projectDesc"
+              required
+              placeholder="Project Description"
+              value={projectDesc}
+              onChange={(e) => setProjectDesc(e.target.value)}
+              className={styles.input}
+            />
+            <button type="submit" className={styles.button}>
+              Create Project
+            </button>
+          </form>
+        )}
+      </div>
     </div>
 
   );
